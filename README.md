535e13e0fd2d282016aacf50decd8995dfd987e8
JudeRosinski <116970121+JudeRosinski@users.noreply.github.com>
2022-11-07 11:06:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CSDS221-Project-2-Rosinski
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-op9hmh)
---
+++ src/App.js
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import './style.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+let nextId = 0;
+
+export default function App() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [deadline, setDeadline] = useState('');
+  const [priority, setPriority] = useState('');
+  const [isComplete, setIsComplete] = useState('');
+  const [Task, setTask] = useState([]);
+
+  function addRow() {
+    setTask([
+      ...Task,
+      {
+        id: nextId++,
+        title: 'Task.title',
+        description: 'Task.setDescription',
+        deadline: 'Task.deadline',
+        priority: Task.priority,
+        isComplete: Task.isComplete,
+      },
+    ]);
+  }
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Container maxWidth="100%">
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+      <AppBar position="static">
+        <Toolbar style={{ align: 'center' }}>
+          <Typography
+            align="center"
+            style={{ width: '100%', alignItems: 'center' }}
+          >
+            <Grid xs display="flex" justifyContent="center" alignItems="center">
+              <MenuIcon />
+              &nbsp;FRAMEWORKS
+            </Grid>
+          </Typography>
+          <Button
+            variant="contained"
+            id="addButton"
+            style={{ width: '7%' }}
+            onClick={() => {
+              addRow();
+              handleClickOpen();
+            }}
+          >
+            <AddCircleIcon fontSize="small" />
+            &nbsp;Add
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Title</TableCell>
+            <TableCell align="center">Description</TableCell>
+            <TableCell align="center">Deadline</TableCell>
+            <TableCell align="center">Priority</TableCell>
+            <TableCell align="center">Is Complete</TableCell>
+            <TableCell align="center">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Task.map((Task) => (
+            <TableRow key={Task.id}>
+              <TableCell align="center">{Task.title}</TableCell>
+              <TableCell align="center">{Task.description}</TableCell>
+              <TableCell align="center">{Task.deadline}</TableCell>
+              <TableCell align="center">{Task.priority}</TableCell>
+              <TableCell align="center">{Task.isComplete}</TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Container>
+  );
+}
